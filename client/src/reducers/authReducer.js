4e904a0fb5305a29cbd88e0fe0bcc1ecbabb471c
1stfaccia1 <chad.m.wittman75@gmail.com>
2019-04-13 01:21:51 -0500

    reducers

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,14 @@
+cont initailState = {
+isAuthenticated: false,
+user: {}
+}
+
+export default function(state = initailState, action){
+  switch (action.type) {
+    // case expression:
+    //
+    //   break;
+    default:
+      return state;
+  }
+}
--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,7 @@
 import {createStore,applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+
+const middleware = {thunk};
 
 const store = createStore(() => [], {}, applyMiddleware());
 
