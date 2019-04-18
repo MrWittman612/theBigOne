c1a9ebdedf477ebf206ed3456902c6c5251b3b40
1stfaccia1 <chad.m.wittman75@gmail.com>
2019-04-17 19:38:58 -0500

    addd

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profileActions';
 import Spinner from '../common/Spinner';
+import ProfileActions from './ProfileActions';
 
 class Dashboard extends Component {
   componentDidMount() {
@@ -19,7 +20,11 @@ if (profile === null || loading) {
   dashboardContent = <Spinner />;
 } else {
   if (Object.keys(profile).length > 0) {
-    dashboardContent = <h4>dispaly profile</h4>;
+    dashboardContent = (
+      <div>
+        <p className="lead text-muted">Welcome <Link to={`/profile/${profile.handle}`}>{user.name}</Link></p>
+      </div>
+    );
   } else {
     dashboardContent = (
       <div>
