af65fb164f2ff9969da280f63cd1f17cdfe3f9ec
1stfaccia1 <chad.m.wittman75@gmail.com>
2019-04-22 18:42:10 -0500

    make it better-12

--- client/src/store.js
+++ client/src/store.js
@@ -1,15 +1,31 @@
-import {createStore,applyMiddleware,compose} from 'redux';
+// import {createStore,applyMiddleware,compose} from 'redux';
+// import thunk from 'redux-thunk';
+// import rootReducer from './reducers'
+//
+// const initialState = {};
+//
+// const middleware = [thunk];
+//
+// const store = createStore(rootReducer, initialState, compose(
+//   applyMiddleware(...middleware),
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//
+// ))
+//
+// export default store;
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
 import thunk from 'redux-thunk';
-import rootReducer from './reducers'
+import rootReducer from './reducers';
 
 const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, initialState, compose(
-  applyMiddleware(...middleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-
-))
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export default store;
