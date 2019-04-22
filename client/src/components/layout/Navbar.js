7636086de792994b833a8d63580ef64758bd24bf
1stfaccia1 <chad.m.wittman75@gmail.com>
2019-04-22 08:09:23 -0500

    make it better

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -63,7 +63,7 @@ const guestLinks = (
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
              <div className="container">
                <Link className="navbar-brand" to="/">
-                 DevConnector
+                 MPLS DevConnect
                </Link>
                <button
                  className="navbar-toggler"
