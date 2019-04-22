d149927a461f9dcb8223451f6e2c5802c7766dbf
1stfaccia1 <chad.m.wittman75@gmail.com>
2019-04-22 11:53:39 -0500

    make it better-9

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use('/static', express.static(path.join(__dirname, 'client/build')));
-  // app.use(express.static('client/build'));
+  // app.use('/static', express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
