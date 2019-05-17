6f5101625ce17b5f05a310100eb029f32f3ef477
1sfaccia1 <chad.m.wittman75@gmail.com>
2019-05-17 18:40:06 -0500

    fix link on github repos

--- config/dev_keys.js
+++ config/dev_keys.js
@@ -1,6 +1,7 @@
 module.exports = {
-  mongoURI:'mongodb+srv://theApp:0ebECvxPbdFIRcr9dYWg@cluster0-lzqmj.mongodb.net/theBigOne?retryWrites=true',
-  secretOrKey: 'secret',
-  githubCientId: 'da2db6f4ef18fc3db40f',
-  githubClientSecret: '8b00e3f7c6dd0e80414c1d860a8d1336d9f05bc0',
+	mongoURI:
+		'mongodb+srv://theApp:0ebECvxPbdFIRcr9dYWg@cluster0-lzqmj.mongodb.net/theBigOne?retryWrites=true',
+	secretOrKey: 'secret',
+	githubClientId: 'da2db6f4ef18fc3db40f',
+	githubClientSecret: '8b00e3f7c6dd0e80414c1d860a8d1336d9f05bc0'
 };
