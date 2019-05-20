a3adc686fb59bfee5788e036ab19969e6c2a680d
1sfaccia1 <chad.m.wittman75@gmail.com>
2019-05-19 21:56:28 -0500

    added

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -45,42 +45,6 @@ class Login extends Component {
 		this.setState({ [e.target.name]: e.target.value });
 	};
 
-<<<<<<< HEAD
-  render () {
-    const {errors} = this.state;
-    return (
-      <div className="login">
-       <div className="container">
-         <div className="row">
-           <div className="col-md-8 m-auto">
-             <h1 className="display-4 text-center">Log In</h1>
-             <p className="lead text-center">Sign in to your DevConnector account</p>
-             <form noValidate onSubmit={this.onSubmit}>
-               <TextFieldGroup
-                 placeholder="Email Address"
-                 name="email"
-                 type="email"
-                 value={this.state.email}
-                 onChange={this.onChange}
-                 error={errors.email}
-                />
-                <TextFieldGroup
-                   placeholder="Password"
-                   name="password"
-                   type="password"
-                   value={this.state.password}
-                   onChange={this.onChange}
-                   error={errors.password}
-                 />
-                 <input type="submit" className="btn btn-info btn-block mt-4" />
-             </form>
-           </div>
-         </div>
-       </div>
-     </div>
-   );
-  }
-=======
 	render() {
 		const { errors } = this.state;
 		return (
@@ -111,7 +75,6 @@ class Login extends Component {
 			</div>
 		);
 	}
->>>>>>> master
 }
 
 Login.propTypes = {
