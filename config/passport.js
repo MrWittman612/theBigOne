886076cd602d70b1169398a8c5ffbb17dc86d0b9
1sfaccia1 <chad.m.wittman75@gmail.com>
2019-05-22 23:09:37 -0500

    uncommit console.log

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,7 @@
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
-const User = mongoose.model("users");
+const User = mongoose.model('users');
 const keys = require('../config/keys');
 
 const opts = {};
@@ -9,16 +9,17 @@ opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = keys.secretOrKey;
 
 module.exports = passport => {
-  passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-    console.log(jwt_payload);
-      User.findById(jwt_payload.id)
-      .then(user => {
-        if (user) {
-          return done(null, user);
-        }
-        return done(null, false);
-      })
-      .catch(err => console.log(err));
-    })
-  );
+	passport.use(
+		new JwtStrategy(opts, (jwt_payload, done) => {
+			// console.log(jwt_payload);
+			User.findById(jwt_payload.id)
+				.then(user => {
+					if (user) {
+						return done(null, user);
+					}
+					return done(null, false);
+				})
+				.catch(err => console.log(err));
+		})
+	);
 };
