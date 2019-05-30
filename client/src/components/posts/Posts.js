3a55439c6b50603a6321d3b8d67baa6634e42ec9
1sfaccia1 <chad.m.wittman75@gmail.com>
2019-05-30 03:23:00 -0500

    fixed css posts feed

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,46 +1,50 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import PostForm from './PostForm';
 import PostFeed from './PostFeed';
 import Spinner from '../common/Spinner';
-import {getPosts} from '../../actions/postActions';
+import { getPosts } from '../../actions/postActions';
 
+import './posts.css';
 class Posts extends Component {
-  componentDidMount() {
-    this.props.getPosts();
-  }
-render() {
-  const {posts,loading} = this.props.post;
-    let postContent;
-    if (posts === null || loading) {
-      postContent = <Spinner />
-    } else {
-      postContent = <PostFeed posts={posts} />;
-    }
+	componentDidMount() {
+		this.props.getPosts();
+	}
+	render() {
+		const { posts, loading } = this.props.post;
+		let postContent;
+		if (posts === null || loading) {
+			postContent = <Spinner />;
+		} else {
+			postContent = <PostFeed posts={posts} />;
+		}
 
-  return (
-    <div className="feed">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <PostForm />
-            {postContent}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+		return (
+			<div className="feed" id="postFeed-mainContent">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-12">
+							<PostForm />
+							{postContent}
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 Posts.propTypes = {
-  getPosts: PropTypes.func.isRequired,
-  post: PropTypes.object.isRequired
-}
+	getPosts: PropTypes.func.isRequired,
+	post: PropTypes.object.isRequired
+};
 
 const mapStateToProps = state => ({
-  post: state.post
-})
+	post: state.post
+});
 
-export default connect(mapStateToProps,{getPosts})(Posts);
+export default connect(
+	mapStateToProps,
+	{ getPosts }
+)(Posts);
