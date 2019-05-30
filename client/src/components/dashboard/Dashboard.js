dfd38ee0195403ac60933bf25b56acdb065458a8
1sfaccia1 <chad.m.wittman75@gmail.com>
2019-05-30 01:53:35 -0500

    toke out h1

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -29,9 +29,9 @@ class Dashboard extends Component {
 			if (Object.keys(profile).length > 0) {
 				dashboardContent = (
 					<div>
-						<p className="lead text-muted">
+						<h1 className="display-4 lead text-muted">
 							Welcome <Link to={`/profile/${profile.handle}`}>{user.name}</Link>
-						</p>
+						</h1>
 						<ProfileActions />
 						<Experience experience={profile.experience} />
 						<Education education={profile.education} />
@@ -47,7 +47,7 @@ class Dashboard extends Component {
 			} else {
 				dashboardContent = (
 					<div>
-						<p className="lead text-muted">Welcome {user.name}</p>
+						<h1 className="display-4 lead text-muted">Welcome {user.name}</h1>
 						<p>You have not yet setup a profile, please add some info</p>
 						<Link to="/create-profile" className="btn btn-lg btn-info">
 							Create Profile
@@ -62,7 +62,7 @@ class Dashboard extends Component {
 				<div className="container">
 					<div className="row">
 						<div className="col-md-12">
-							<h1 className="display-4">Dashboard</h1>
+							{/* <h1 className="display-4">Dashboard</h1> */}
 							{dashboardContent}
 						</div>
 					</div>
